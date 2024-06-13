1859ce7531cef300aa3e081fd5a3df0b5e87eab1
KmK33 <kmk112003@gmail.com>
2024-06-13 16:23:09 +0530

    deletion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 struct Node {
     value:i32,
     next:Option<Box<Node>>
@@ -8,51 +9,74 @@ struct LinkList{
 }
 
 impl LinkList {
-    fn new() -> Self{
-        LinkList {head :None}
+    fn new() -> Self {
+        LinkList {head:None}
     }
 
-    fn append(&mut self,value:i32) {
-        let new_node = Box::new(Node{value,next:None});
+    fn append(&mut self,value:i32){
+        let new_node = Box::new(Node {value,next:None});
 
         match self.head{
-            Some(ref mut node) => {
-                let mut current_node = node;
-                while let Some(ref mut next_node) = current_node.next{
-                    current_node = next_node;
+            Some(ref mut node) =>{
+                let mut current = node;
+
+                while let Some(ref mut next_node) = current.next{
+                    current = next_node;
                 }
-                current_node.next = Some(new_node);
+
+                current.next = Some(new_node);
             },
             None => {
-                self.head = Some(new_node); 
+                self.head = Some(new_node);
             }
         }
     }
 
-    fn search_node(&mut self,target:i32) -> Option<i32>{
-        let mut current =  &self.head;
+    fn delete_node(&mut self,target:i32) {
 
-        while let Some(ref  node) = current {
+        if let Some(ref mut head) = self.head{
+            if head.value == target{
+                self.head = head.next.take();
+                return;
+            };
+        };
+
+        let mut current = &mut self.head;
+
+        while let Some(ref mut node) = current  {
+            if let Some(ref mut next_node) = node.next {
+                if next_node.value == target{
+                    node.next = next_node.next.take();
+                    return;
+                }
+            }
+            current = &mut node.next;
+        }
+       
+    }
+
+    fn search_node(&self,target:i32) -> Option<i32>{
+        let mut current = &self.head;
+
+        while let Some(ref node) = current  {
             if node.value == target{
                 return Some(node.value);
             }
-
             current = &node.next;
         };
         None
+    }
 
     
-    }
 
-    fn traverse(&self) {
+    fn traverse(&self){
         let mut current = &self.head;
-        while let Some(node) = current {
+
+        while let Some(node) = current  {
             println!("{}",node.value);
-            current = &node.next;
+            current = &node.next
         }
-
     }
-
 }
 
 
@@ -72,4 +96,7 @@ fn main() {
     }else {
         println!("Not found ")
     }
+    list.delete_node(3000);
+
+    list.traverse();
 }
